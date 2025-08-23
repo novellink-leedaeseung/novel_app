4b2dac2943a9da59541332d87f49374554c0cc22
lee <eotmd1016@gmail.com>
2025-08-23 14:32:44 +0900

    공지사항 버튼 만들었음

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -43,21 +43,79 @@ class _HomeScreenState extends State<HomeScreen> {
             child: ListView(
               padding: EdgeInsets.zero,
               children: [
-                SizedBox(
-                  height: 90,
-                  child: OutlinedButton(
-                    style: OutlinedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 15,
+                    horizontal: 24,
+                  ),
+                  child: SizedBox(
+                    width: 327,
+                    height: 90,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color(0xff227eff), // 파란색 glow
+                            blurRadius: 4, // 퍼짐 정도 (값 키울수록 부드럽게)
+                            spreadRadius: 0, // 바깥으로 확장
+                          ),
+                        ],
                       ),
-                    ),
-                    onPressed: () {},
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("공지사항"),
-                        Text("근로자의 날 오픈기념으로 이벤트 진행합니다!"),
-                      ],
+                      child: OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          elevation: 3.0,
+                          padding: EdgeInsets.zero,
+                          side: BorderSide(color: Color(0xff227eff)),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                left: 12,
+                                right: 12,
+                                top: 14,
+                              ),
+                              child: SizedBox(
+                                width: double.infinity,
+                                height: 26,
+                                child: Text(
+                                  "공지사항",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                left: 12,
+                                right: 12,
+                                top: 11,
+                              ),
+                              child: SizedBox(
+                                width: double.infinity,
+                                height: 26,
+                                child: Text(
+                                  "근로자의 날 오픈기념으로 이벤트 진행합니다!",
+                                  style: TextStyle(
+                                    color: Color(0xff227eff),
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                   ),
                 ),
