904e0d1428ab73372e06598937c06a6febce6f4d
lee <eotmd1016@gmail.com>
2025-08-25 00:39:01 +0900

    [혈압 상세] - 맥박

--- lib/primary-color.dart
+++ lib/primary-color.dart
@@ -1,8 +1,14 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 
 class PrimaryColor {
   static Color gray = Color(0xff8C8C8C);
   static Color blue = Color(0xff227EFF);
+  static Color pink = Color(0xffFFC0CC);
+  static Color orange = Color(0xffE5621C);
+  static Color white = Colors.white;
+
 
 }
--- lib/screen/blood-pressure-info.dart
+++ lib/screen/blood-pressure-info.dart
@@ -36,13 +36,20 @@ class _BloodPressureInfoState extends State<BloodPressureInfo> {
                       },
                       icon: Icon(Icons.arrow_back_outlined),
                       style: IconButton.styleFrom(
-                        foregroundColor: Colors.white
+                        foregroundColor: Colors.white,
                       ),
                     ),
 
                     Padding(
                       padding: const EdgeInsets.only(left: 125),
-                      child: Text("혈압", style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w700)),
+                      child: Text(
+                        "혈압",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
                     ),
                   ],
                 ),
@@ -55,17 +62,144 @@ class _BloodPressureInfoState extends State<BloodPressureInfo> {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text("2025.08.15", style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600
-                ),),
-                IconButton(onPressed: (){}, icon: Icon(Icons.date_range))
+                Text(
+                  "2025.08.15",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                ),
+                IconButton(onPressed: () {}, icon: Icon(Icons.date_range)),
               ],
             ),
           ),
-
+          // 고혈압 1기
+          Padding(
+            padding: const EdgeInsets.only(left: 24, top: 15),
+            child: SizedBox(
+              child: Row(
+                children: [
+                  // 고혈압 1기, 맥박
+                  Container(
+                    decoration: grayBoxDecoration(),
+                    width: 156,
+                    height: 125,
+                    child: Column(
+                      children: [
+                        Text("고혈압 수치"),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 53, left: 65),
+                          child: Text(
+                            "고혈압 1기",
+                            style: TextStyle(
+                              color: PrimaryColor.orange,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  // 맥박
+                  Padding(
+                    padding: const EdgeInsets.only(left: 15, right: 24),
+                    child: Container(
+                      decoration: grayBoxDecoration(),
+                      width: 156,
+                      height: 125,
+                      child: Column(
+                        children: [
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Padding(
+                              padding: const EdgeInsets.only(top: 10, left: 10),
+                              child: Container(
+                                decoration: heartBoxDecoration(),
+                                width: 38,
+                                height: 38,
+                                child: Image.asset(
+                                  "assets/images/icon/heart.png",
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              top: 16,
+                              bottom: 10,
+                              right: 3,
+                              left: 60,
+                            ),
+                            child: SizedBox(
+                              width: 76,
+                              height: 49,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      Text(
+                                        "70",
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.w700,
+                                        ),
+                                      ),
+                                      Text(
+                                        " bpm",
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.w400,
+                                          fontSize: 14,
+                                          color: PrimaryColor.gray,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  Text(
+                                    "맥박",
+                                    style: TextStyle(
+                                      color: PrimaryColor.gray,
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
+    );
+  }
+
+  BoxDecoration heartBoxDecoration() {
+    return BoxDecoration(
+      color: PrimaryColor.pink,
+      borderRadius: BorderRadius.circular(12),
+    );
+  }
+
+  BoxDecoration grayBoxDecoration() {
+    return BoxDecoration(
+      color: Colors.white,
+      border: Border.all(color: PrimaryColor.white, width: 1),
+      borderRadius: BorderRadius.circular(20), // 모서리 둥글게
+      boxShadow: [
+        BoxShadow(
+          color: PrimaryColor.gray, // 흰색 그림자
+          blurRadius: 4, // 번짐 정도
+          spreadRadius: 0, // 확장 정도 (0이면 깔끔)
+          offset: Offset(0, 2), // X=0, Y=2 → 아래쪽으로만 그림자
+        ),
+      ],
     );
   }
 }
