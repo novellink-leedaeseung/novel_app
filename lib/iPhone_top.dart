04109a088e23dc56882f657258666b1ddaf714c6
lee <eotmd1016@gmail.com>
2025-08-26 00:04:49 +0900

    [피그마 맞출수 있는 라이브러리] 아이폰 노치 수정

--- lib/iPhone_top.dart
+++ lib/iPhone_top.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class IphoneTop extends StatelessWidget {
   const IphoneTop({super.key});
 
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.init(context, designSize: Size(375, 812));
+
     // 다이나믹 아일랜드 및 노치
-    return SizedBox(height: 50);
+    return SizedBox(width: 375.w, height: 44.h);
   }
 }
