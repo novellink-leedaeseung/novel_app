5ad8dced231ad14973b4ce6cb582449cfed84981
lee <eotmd1016@gmail.com>
2025-08-26 00:05:28 +0900

    홈화면 프로필 사진 피그마와 같게 수정

--- lib/component/user-profile.dart
+++ lib/component/user-profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 // 프로필 사진, 사용자 이름, 사람모양, 옵션
 class UserProfile extends StatelessWidget {
@@ -6,69 +7,88 @@ class UserProfile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        margin: EdgeInsets.only(left: 24, right: 24),
-        child: Expanded(
-          child: Row(
-            children: [
-              Row(
-                children: [
-                  ClipOval(
+    ScreenUtil.init(context, designSize: Size(375, 812));
+
+    return Container(
+      margin: EdgeInsets.only(top: 12),
+      width: 327.w,
+      height: 54.h,
+      child: Row(
+        children: [
+          SizedBox(
+            width: 145.w,
+            height: 54.h,
+            child: Row(
+              children: [
+                SizedBox(
+                  width: 54.w,
+                  height: 54.w,
+                  child:ClipOval(
                     child: Image.asset(
                       'assets/images/profile/profile.jpg',
-                      width: 54,
-                      height: 54,
+                      width: 100,
+                      height: 100,
+                      fit: BoxFit.fill,
                     ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.fromLTRB(12, 0, 0, 0),
-                    child: Text(
-                      "노블이",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w500,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Image.asset('assets/images/icon/downward.png'),
-                    color: Colors.white,
-                    iconSize: 14,
-                  ),
-                ],
-              ),
+                  )
 
-              Expanded(
-                // height: 45,
-                // width: MediaQuery.of(context).size.width * 0.4,
-                // margin: EdgeInsets.symmetric(horizontal: 10),
-                child: Center(
+                ),
+                Container(
+                  width: 79.w,
+                  height: 31.h,
+                  margin: EdgeInsets.fromLTRB(12, 11.5, 0, 11.5),
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      IconButton(
-                        onPressed: () {},
-                        icon: Image.asset('assets/images/icon/user-group.png'),
-                        color: Colors.white,
-                        iconSize: 24,
+                      Text(
+                        "노블이",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 20,
+                        ),
                       ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: Image.asset('assets/images/icon/control.png'),
-                        color: Colors.white,
-                        iconSize: 24,
+                      SizedBox(
+                        width: 24.w,
+                        height: 23.25.h,
+                        child: Image.asset(
+                          'assets/images/icon/downward.png',
+                          width: 14,
+                          height: 6.78,
+                        ),
                       ),
                     ],
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
+          Container(
+            margin: EdgeInsets.only(left: 126, right: 8),
+            width: 24.w,
+            height: 24.h,
+            child: IconButton(
+              padding: EdgeInsets.only(),
+              onPressed: () {},
+              icon: Image.asset(
+                'assets/images/icon/user-group.png',
+                width: 24.w,
+                height: 24.h,
+              ),
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(
+            width: 24.w,
+            height: 24.h,
+            child: IconButton(
+              padding: EdgeInsets.zero,
+              onPressed: () {},
+              icon: Image.asset('assets/images/icon/control.png'),
+              color: Colors.white,
+            ),
+          ),
+        ],
       ),
     );
   }
