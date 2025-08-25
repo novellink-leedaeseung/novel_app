67d082456ab09984707d7eea38e4351bce74131e
lee <eotmd1016@gmail.com>
2025-08-26 00:06:29 +0900

    공지사항 배너 "기업회원 전환" 피그마 재디자인이 필요

--- lib/component/welcome-banner.dart
+++ lib/component/welcome-banner.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 
 // 환영메시지, 기업회원 버튼
 class WelcomeBanner extends StatelessWidget {
@@ -6,15 +8,17 @@ class WelcomeBanner extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return               Container(
-      margin: EdgeInsets.only(left: 24, bottom: 14),
+    ScreenUtil.init(context, designSize: Size(375, 812));
+
+    return Container(
+      margin: EdgeInsets.only(left: 24, bottom: 14, top: 14),
       child: Row(
         children: [
           SizedBox(
-            width: 190,
-            height: 42,
+            width: 190.w,
+            height: 42.h,
             child: Text(
-              "메디터치 오신걸 환영합니다. 스마트하게 건강 관리하세요!",
+              "메디터치 오신 걸 환영합니다. 스마트하게 건강 관리하세요!",
               style: TextStyle(
                 fontSize: 16,
                 color: Colors.white,
@@ -23,29 +27,24 @@ class WelcomeBanner extends StatelessWidget {
             ),
           ),
 
-          Expanded(
-            child: Container(
-              margin: EdgeInsets.only(left: 35),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Image.asset(
-                    "assets/images/icon/office.png",
-                    width: 24,
-                    height: 24,
+          SizedBox(
+            width: 88.w,
+            height: 24.h,
+            child: Row(
+              children: [
+                Image.asset(
+                  "assets/images/icon/office.png",
+                  width: 24.w,
+                  height: 24.h,
+                ),
+                Text(
+                  "기업회원 전환",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
                   ),
-                  TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      "기업회원 전환",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ],
