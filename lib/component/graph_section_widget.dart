7fe28f26da690ccc90e11d194db1a1bfb7128e7d
lee <eotmd1016@gmail.com>
2025-08-29 11:28:43 +0900

    혈압 페이지 그래프 틀만 잡았음

--- lib/component/graph_section_widget.dart
+++ lib/component/graph_section_widget.dart
@@ -1,18 +1,56 @@
 // lib/screen/bp/graph_section.dart
 import 'package:flutter/material.dart';
 import 'package:novel/primary-color.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:syncfusion_flutter_charts/sparkcharts.dart';
+
+class _SalesData {
+  _SalesData(this.year, this.sales);
+  final String year;
+  final double sales;
+}
+
+final List<_SalesData> data = [
+  _SalesData('Jan', 35),
+  _SalesData('Feb', 28),
+  _SalesData('Mar', 34),
+  _SalesData('Apr', 32),
+  _SalesData('May', 40),
+];
+
 
 class GraphSection extends StatelessWidget {
   const GraphSection({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 30),
-      child: SizedBox(
-        width: double.infinity,
-        height: 210,
-        child: ColoredBox(color: PrimaryColor.cloudyGray),
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      height: 210,
+      color: Colors.white, // 배경색
+      child: SafeArea(
+        child: Column(
+          children: [
+            // Cartesian 차트
+            Expanded(
+              child: SfCartesianChart(
+                primaryXAxis: CategoryAxis(),
+                legend: Legend(isVisible: true),
+                tooltipBehavior: TooltipBehavior(enable: true),
+                series: <CartesianSeries<_SalesData, String>>[
+                  LineSeries<_SalesData, String>(
+                    dataSource: data,
+                    xValueMapper: (s, _) => s.year,
+                    yValueMapper: (s, _) => s.sales,
+                    name: 'Sales',
+                    dataLabelSettings: const DataLabelSettings(isVisible: true),
+                  ),
+                ],
+              ),
+            ),
+
+          ],
+        ),
       ),
     );
   }
